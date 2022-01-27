4de42e54610aefe2cc8ef4981b190f3f6c3a5a1e
Vanesa Medina <mv382038@gmail.com>
2022-01-26 23:03:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function randomNumbers (){
+    return Math.floor(Math.random() * 20);
+}
+
+console.log(randomNumbers())
+
+
+for(let i = 0; i < 10001; i++){
+    const obj = {i}
+    console.log(obj)
+}
+
